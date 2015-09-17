e90394d5609432bdd913867476824361fc6bb59d
Donald Plummer <dplummer@avvo.com>
2015-09-17 11:13:27 -0700

    Record cache times

--- lib/overall_request_times/railtie.rb
+++ lib/overall_request_times/railtie.rb
@@ -2,6 +2,12 @@ module OverallRequestTimes
   class Railtie < Rails::Railtie
     initializer "overall_request_times.configure_rails_initialization" do |app|
       app.middleware.use OverallRequestTimes::RailsMiddleware
+
+      [:read, :write, :delete, :fetch_hit].each do |name|
+        ActiveSupport::Notifications.subscribe("cache_#{name}.active_support") do |_name, start, ending, _id, _payload|
+          OverallRequestTimes.add_duration(:cache, ending - start)
+        end
+      end
     end
   end
 end
