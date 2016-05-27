8b169ed96fdf97c778d89957e040fdcd595ed42f
Donald Plummer <dplummer@avvo.com>
2016-05-27 11:10:43 -0700

    Remove double counting fetch hits
    A fetch hit will be recorded as a cache_read, so we don't need to record
    it too.

--- lib/overall_request_times/railtie.rb
+++ lib/overall_request_times/railtie.rb
@@ -3,7 +3,7 @@ module OverallRequestTimes
     initializer "overall_request_times.configure_rails_initialization" do |app|
       app.middleware.use OverallRequestTimes::RailsMiddleware
 
-      [:read, :write, :delete, :fetch_hit].each do |name|
+      [:read, :write, :delete].each do |name|
         ActiveSupport::Notifications.subscribe("cache_#{name}.active_support") do |_name, start, ending, _id, _payload|
           OverallRequestTimes.add_duration(:cache, ending - start)
         end
