c80ec146fecdf4f7d9758cf2fcdc40300e4cd96f
Donald Plummer <dplummer@avvo.com>
2016-05-27 11:16:54 -0700

    Log when cache operation takes too long

--- lib/overall_request_times/railtie.rb
+++ lib/overall_request_times/railtie.rb
@@ -3,8 +3,12 @@ module OverallRequestTimes
     initializer "overall_request_times.configure_rails_initialization" do |app|
       app.middleware.use OverallRequestTimes::RailsMiddleware
 
-      ActiveSupport::Notifications.subscribe(/cache_(read|write|delete).active_support/) do |_name, start, finish, _id, _payload|
-        OverallRequestTimes.add_duration(:cache, finish - start)
+      ActiveSupport::Notifications.subscribe(/cache_(read|write|delete).active_support/) do |name, start, finish, _id, payload|
+        duration_in_seconds = finish - start
+        if duration_in_seconds > 0.005
+          Rails.logger.info "Cache operation took over 5ms: name:#{name} start:#{start.to_f} finish:#{finish.to_f} payload:#{payload.inspect}"
+        end
+        OverallRequestTimes.add_duration(:cache, duration_in_seconds)
       end
     end
   end
