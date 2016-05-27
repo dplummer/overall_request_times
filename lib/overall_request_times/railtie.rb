99e45902944c993dfe1e10986ddb37459abe3b4e
Donald Plummer <dplummer@avvo.com>
2016-05-27 12:35:26 -0700

    Remove slow cache logging

--- lib/overall_request_times/railtie.rb
+++ lib/overall_request_times/railtie.rb
@@ -4,11 +4,7 @@ module OverallRequestTimes
       app.middleware.use OverallRequestTimes::RailsMiddleware
 
       ActiveSupport::Notifications.subscribe(/cache_(read|write|delete).active_support/) do |name, start, finish, _id, payload|
-        duration_in_seconds = finish - start
-        if duration_in_seconds > 0.005
-          Rails.logger.info "Cache operation took over 5ms: name:#{name} start:#{start.to_f} finish:#{finish.to_f} payload:#{payload.inspect}"
-        end
-        OverallRequestTimes.add_duration(:cache, duration_in_seconds)
+        OverallRequestTimes.add_duration(:cache, finish - start)
       end
     end
   end
